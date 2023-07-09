1eab1e69ade85abcaedbcf6dd71fea874740ed66
DEBANJAN MUKHERJEE <113274631+RickDeb2004@users.noreply.github.com>
2023-07-09 08:16:33 +0000

    commited in app,jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Debanjan Mukherjee",
+  title: "Open Source Contributor & Backend Developer",
+  email: "debanjanrick04@gmail.com",
+  gitHub: "RickDeb2004",
+  instagram: "i_am_debanjan_2004",
+  linkedIn: "debanjan-mukherjee-181615224",
+  
+  
 };
 
 const primaryColor = "#4E567E";

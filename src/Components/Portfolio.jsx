272576ecbc85655cab606570156080bd00cc7d60
DEBANJAN MUKHERJEE <113274631+RickDeb2004@users.noreply.github.com>
2023-07-09 08:08:39 +0000

    portfolio.jsx updated

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,29 +31,36 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
-    description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
-  },
-  {
-    title: "Web Development for Beginners",
-    description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
-  },
-  {
-    title: "My Resume Site",
-    description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
-  },
+    title: "EvoLoadBalancer",
+ techstack:"Go,System Design Fundamentals",
+   description: "It's basically a load-balancer which has some advance feature with improved load balancing algorithm and having health awareness of servers",
+   url: "https://github.com/RickDeb2004/EvoLoadBalancer",
+ },
+ {
+   title: "PowerCache",
+     techstack:"Go ,System Design Fundamentals",
+   description: "It's a cache server which has some advance feature with improved caching algorithm and having health awareness of servers",
+   url: "https://github.com/RickDeb2004/PowerCache ",
+ },
+ {
+   title: "securE-mail",
+     techstack:"Go",
+   description: "It works as email verification system which is used to verify the email address of the user and domain of the mails and also check the personal info about the owner of the mail ",
+   url:"https://github.com/RickDeb2004/securE-mail"
+ },
+ {
+   title: "FireDataBase",
+     techstack:"Firebase, MERN,REST API ",
+   description: "Making Database using Firebase",
+   url: "https://github.com/RickDeb2004/SPAMBYTES-HACKFEST/tree/master",
+ },
+ {
+     title: "MERN-Stack-Project",
+     techstack:"MERN",
+     description: "A MERN Stack Project",
+     url: "https://github.com/RickDeb2004/MONDAY-MORNING-_TASK-BACKEND",
+
+ }
 ];
 
 const Portfolio = () => {
